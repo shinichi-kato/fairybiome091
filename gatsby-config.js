2a1ec085609116a82ec24e6f34068e10985bfd62
shinichi <skato21r@iris.eonet.ne.jp>
2023-02-05 00:36:36 +0900

    first

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+require("dotenv").config({
+  path: '.env.local',
+});
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-top-layout',
+    'gatsby-plugin-react-helmet',
+    // If you want to use styled components you should add the plugin here.
+    // 'gatsby-plugin-styled-components',
+    'gatsby-plugin-mui-emotion',
+  ],
+  siteMetadata: {
+    title: 'My page',
+  },
+};
